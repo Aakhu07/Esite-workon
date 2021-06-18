23139a043b8db6dc5bad00ed3d70d6576753083d
Aakash-Shah2052 <aakashshah9998@gmail.com>
2021-06-18 12:53:13 +0545

    updated(2)

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", HomeView.as_view(), name ="home"),
     path("all-products/", AllProductsView.as_view(), name ="allproducts"),
     path("products/<slug:slug>/", ProductDetailView.as_view(), name ="productdetail"),
-
+    path("add-to-cart-<int:pro_id>/", AddToCartView.as_view(), name ="addtocart"),
+    path("my-cart/", MyCartView.as_view(), name ="mycart"),
+    path("manage-cart/<int:cp_id>/", ManageCartView.as_view(), name ="managecart"),
 ]
--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import View, TemplateView
 from .models import *
 
 class HomeView(TemplateView):
@@ -28,3 +28,79 @@ class ProductDetailView(TemplateView):
         product.save()
         context['product'] = product
         return context
+
+class AddToCartView(TemplateView):
+    template_name = "addtocart.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+# get product id from requested url
+        product_id = self.kwargs['pro_id']
+# get product
+        product_obj = Product.objects.get(id=product_id)
+# check if cart exits
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+            this_product_in_cart = cart_obj.cartproduct_set.filter(product=product_obj)
+#item already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct =  this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.selling_price
+                cartproduct.save()
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+#new item is added in cart
+            else:
+                cartproduct = CartProduct.objects.create(cart=cart_obj,product=product_obj,rate=product_obj.selling_price,
+                quantity=1,subtotal=product_obj.selling_price)
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()    
+        else:
+            cart_obj = Cart.objects.create(total = 0)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct = CartProduct.objects.create(cart= cart_obj,product=product_obj,rate=product_obj.selling_price,
+                quantity=1,subtotal=product_obj.selling_price)
+            cart_obj.total += product_obj.selling_price
+            cart_obj.save()                         
+        return context
+
+class MyCartView(TemplateView):
+    template_name = "mycart.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id",None)
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart        
+        return context
+
+class ManageCartView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        cp_id = self.kwargs["cp_id"]
+        action = request.GET.get("action")
+        cp_obj = CartProduct.objects.get(id=cp_id)
+        cart_obj = cp_obj.cart 
+        if action == "inc":
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action == "dcr":
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity == 0:
+                cp_obj.delete()
+        elif action == "rmv":
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass    
+        return redirect("ecomapp:mycart")
